dbd91c98519fa1147eeee5fcdebc7f906e236951
latiah <kimtifah2@gmail.com>
2024-07-02 01:18:45 +0200

    Payment with flutterwave

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,55 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {PayWithFlutterwave} from 'flutterwave-react-native';
+import {View, TextInput, Text} from "react-native";
+import {useState} from "react";
+import {router} from "expo-router"
+interface RedirectParams {
+    status: 'successful' | 'cancelled';
+    transaction_id?: string;
+    tx_ref: string;
+  }
+export default function Payment(){
+  const flutterKey = process.env.EXPO_PUBLIC_FLUTTERWAVE_PUBLIC_KEY ?? "";
+  const[email, setEmail]=useState("");
+  const[total, setAmount]=useState("");
+  const[name, setName]=useState("");
+  const handleOnRedirect = (data: RedirectParams) => {
+      console.log(data);
+setAmount("");
+setEmail("");
+setName("");
+    };
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+  const generateTransactionRef = (length: number) => {
+    var result = '';
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return `flw_tx_ref_${result}`;
+  };
+return(
+  <View style={{flex:1,justifyContent:"center"}}>
+    <View style={{margin:20}}>
+    <Text style={{marginBottom:20,fontSize:18}}>Fill in the following details to proceed with paying</Text>
+    <TextInput style={{width:"100%",marginBottom:20 ,borderWidth:1, borderColor:"gray", borderRadius:10,padding:8}} placeholder="enter your email" value={email} onChangeText={(val)=>setEmail(val)}></TextInput>
+    <TextInput style={{width:"100%",marginBottom:20 ,borderWidth:1, borderColor:"gray", borderRadius:10,padding:8}} placeholder="enter your name" value={name} onChangeText={(val)=>setName(val)}></TextInput>
+    <TextInput style={{width:"100%" ,marginBottom:20,borderWidth:1, borderColor:"gray", borderRadius:10,padding:8}} placeholder="enter amount " value={total} onChangeText={(val)=>setAmount(val)} keyboardType='numeric'></TextInput>
+<PayWithFlutterwave
+  onRedirect={handleOnRedirect}
+  options={{
+    tx_ref: generateTransactionRef(10),
+    authorization:flutterKey,
+    customer: {
+      email,
+      name
+    },
+    amount:parseFloat(total),
+    currency: 'RWF',
+    payment_options: 'card'
+  }}
+/>
+</View>
+</View>)
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
